4ad112c45887969cb7e04e475a5f41bca4e896bd
Facundo Cersosimo <cherso@Facundos-MacBook-Pro.local>
2018-10-17 08:46:55 -0300

    subo para adrogue

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  secret: 'poronga',
+  secret: 'facundo',
   store: new MongoStore({
     // url: 'mongodb://localhost/proveedores',
     url: 'mongodb://'+DB.user+':'+DB.pass+'@'+DB.port+'/'+DB.dbName+'',
--- routes/alta.js
+++ routes/alta.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Proveedor = require('../models/modelProveedor').Proveedor;
 var md5 = require('md5');
+const nodemailer = require('nodemailer');
 
 exports.mostrarForm = function(req, res, next) {
   res.render('alta');
@@ -26,6 +27,38 @@ exports.recibirForm = function(req, res, next) {
         if(err) {
            res.send('Ocurrio un error al intentar guardar proveedor en base de datos, intente nuevamente mas tarde');
         } else { 
+
+// Generate test SMTP service account from ethereal.email
+// Only needed if you don't have a real mail account for testing
+nodemailer.createTestAccount((err, account) => {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+             user: 'cersosimof88@gmail.com',
+             pass: 'huevodepascua'
+         }
+     });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Base Proveedores" <cersosimof88@gmail.com>', // sender address
+        to: req.body.correo, // list of receivers
+        bcc: 'cersosimo.facundo@gmail.com',
+        subject: '¡Inscripción exitosa! ✔', // Subject line
+        // text: 'Hello world?', // plain text body
+        html: '<b>Bienvenido ' + req.body.nombre +'!</b><br><p>Gracias por inscribirse a la base de proveedores.</p><br>' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
+});
           res.render('alta' )
         }
       });
