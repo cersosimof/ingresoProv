bd9407b59c55ed99705733be7f90b86542cf749c
Facundo Cersosimo <cherso@Facundos-MacBook-Pro.local>
2018-10-22 18:23:44 -0300

    sda

--- db.js
+++ db.js
@@ -1,11 +1,9 @@
 var db = {
-    user: process.env.USER,
-    pass: process.env.PASS,
-    port: process.env.PORT, 
-    dbName: process.env.DBNAME,
-    mailUser: process.env.MAILUSER,
-    mailPass: process.env.MAILPASS 
+    user: process.env.USER || 'cherso88',
+    pass: process.env.PASS || 'espora1436',
+    port: process.env.PORT || 'ds213472.mlab.com:13472',
+    dbName: process.env.DBNAME || 'provcrud',
+    mailUser: process.env.MAILUSER || 'cersosimof88@gmail.com',
+    mailPass: process.env.MAILPASS || 'huevodepascua'
 }
 module.exports = db;
-
-// hola soy un comentario al pedo
